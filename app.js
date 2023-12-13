80688ccc43ca3673b74e2f6c35bfc65ef4d43eb7
Leslie <416317444@qq.com>
2023-12-13 17:19:09 +0800

    initial

--- app.js
+++ app.js
@@ -19,15 +19,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// 捕捉404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// 错误处理
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
