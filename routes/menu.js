754841195edf29a2db27a00a99aed397cf3264fe
Leslie <416317444@qq.com>
2024-01-09 20:44:32 +0800

    feat:菜单增删改接口完成

--- routes/menu.js
+++ routes/menu.js
@@ -7,7 +7,6 @@ const { buildMenuTree } = require("../utils/index");
 router.post("/createMenu", function (req, res) {
   //获取菜单信息
   const menuInfo = req.body;
-  console.log(req.body);
   //创建菜单,将parentId为0转换成null
   if (menuInfo.parentId === 0) {
     menuInfo.parentId = null;
@@ -30,9 +29,34 @@ router.get("/getMenuList", async function (req, res) {
   });
 });
 
+/* 编辑菜单 */
+router.post("/editMenu", async function (req, res) {
+  const { id, menuInfo } = req.body;
+  await Menu.findByIdAndUpdate(id, { $set: menuInfo });
+  return res.send({
+    status: 200,
+    message: "编辑成功",
+  });
+});
+
 /* 删除菜单 */
-router.post("/deleteMenu",async function (req,res) {
-  
-})
+router.post("/deleteMenu", async function (req, res) {
+  const { id } = req.body;
+  deleteMenuAndChildren(id);
+  return res.send({
+    status: 200,
+    message: "删除成功",
+  });
+});
+
+async function deleteMenuAndChildren(targetId) {
+  // 删除该document
+  await Menu.deleteOne({ _id: targetId });
+  //查找所有子内容，递归删除
+  const children = await Menu.find({ parentId: targetId });
+  for (const child of children) {
+    await deleteMenuAndChildren(child._id);
+  }
+}
 
 module.exports = router;
