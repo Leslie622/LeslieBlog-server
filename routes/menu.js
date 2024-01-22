cb1727b6688d43c9e2b7b9b198595891724f7f35
Leslie <416317444@qq.com>
2024-01-22 14:08:07 +0800

    chore:修改menu接口

--- routes/menu.js
+++ routes/menu.js
@@ -4,17 +4,18 @@ const Menu = require("../models/menuSchema");
 const { buildMenuTree, transformMenuList } = require("../utils/index");
 
 /* 创建菜单 */
-router.post("/createMenu", function (req, res) {
+router.post("/createMenu", async function (req, res) {
   //获取菜单信息
   const menuInfo = req.body;
   //创建菜单,将parentId为0转换成null
   if (menuInfo.parentId === 0) {
     menuInfo.parentId = null;
   }
-  Menu.create(menuInfo);
+  await Menu.create(menuInfo);
   return res.send({
     status: 200,
     message: "创建成功",
+    data: {},
   });
 });
 
@@ -31,11 +32,12 @@ router.get("/getMenuList", async function (req, res) {
 
 /* 编辑菜单 */
 router.post("/editMenu", async function (req, res) {
-  const { id, menuInfo } = req.body;
+  const { id, ...menuInfo } = req.body;
   await Menu.findByIdAndUpdate(id, { $set: menuInfo });
   return res.send({
     status: 200,
     message: "编辑成功",
+    data: {},
   });
 });
 
@@ -46,6 +48,7 @@ router.post("/deleteMenu", async function (req, res) {
   return res.send({
     status: 200,
     message: "删除成功",
+    data: {},
   });
 });
 
