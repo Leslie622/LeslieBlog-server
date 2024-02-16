5b3b347221abcff7219a11e1e4c3660b1939bdf1
Leslie <416317444@qq.com>
2024-02-16 19:31:41 +0800

    chore:数据库配置

--- db/index.js
+++ db/index.js
@@ -1,18 +1,21 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dbConfig = require("../config/db");
 
 //数据库地址
 const dbURL = "mongodb://localhost:27017/leslie-blog";
 
 //连接MongoDB数据库
 async function main() {
-  await mongoose.connect(dbURL);
+  await mongoose.connect(dbURL, {
+    user: dbConfig.user,
+    pass: dbConfig.pass,
+  });
 }
 
 //连接成功回调
 main().then(() => {
   //...
-  
-})
+});
 
 //连接失败回调
 main().catch(() => {
