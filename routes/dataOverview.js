226d0c428d862a55ee0910d65d5cedb11a81fcd3
Leslie <416317444@qq.com>
2024-03-22 15:55:48 +0800

    fix:修复一个bug，曾导致游客地理位置数据错误

--- routes/dataOverview.js
+++ routes/dataOverview.js
@@ -4,15 +4,15 @@ const https = require("https");
 const Visitor = require("../models/visitorSchema");
 
 const defaultMapData = [
-  { name: "北京市", value: 0 },
-  { name: "天津市", value: 0 },
+  { name: "北京", value: 0 },
+  { name: "天津", value: 0 },
   { name: "河北省", value: 0 },
   { name: "山西省", value: 0 },
   { name: "内蒙古", value: 0 },
   { name: "辽宁省", value: 0 },
   { name: "吉林省", value: 0 },
   { name: "黑龙江省", value: 0 },
-  { name: "上海市", value: 0 },
+  { name: "上海", value: 0 },
   { name: "江苏省", value: 0 },
   { name: "浙江省", value: 0 },
   { name: "安徽省", value: 0 },
@@ -25,7 +25,7 @@ const defaultMapData = [
   { name: "广东省", value: 0 },
   { name: "广西", value: 0 },
   { name: "海南省", value: 0 },
-  { name: "重庆市", value: 0 },
+  { name: "重庆", value: 0 },
   { name: "四川省", value: 0 },
   { name: "贵州省", value: 0 },
   { name: "云南省", value: 0 },
@@ -99,14 +99,19 @@ router.get("/getVisitorLocation", async function (req, res) {
 /* 工具函数：将两个对象数组合并 */
 function mergeArrays(arr1, arr2) {
   const merged = {};
-  for (const item1 of arr1) {
-    merged[item1.name] = item1.value;
-  }
-  for (const item2 of arr2) {
-    if (item2.name !== "") {
-      merged[item2.name] = item2.value;
+  // 合并数组1中的元素
+  arr1.forEach((item) => {
+    const { name, value } = item;
+    merged[name] = (merged[name] || 0) + value;
+  });
+  // 合并数组2中的元素，但只保留第一个数组中存在的名称
+  arr2.forEach((item) => {
+    const { name, value } = item;
+    if (merged.hasOwnProperty(name)) {
+      merged[name] += value;
     }
-  }
+  });
+  // 构建合并后的结果数组
   const result = Object.entries(merged).map(([name, value]) => ({ name, value }));
   return result;
 }
